d39ae3a05a728a2da6206ef1045cd92974f67ef0
Vincent Wang <376787823@qq.com>
2025-08-06 17:37:48 +0800

    Add files via upload
    add cors

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
 from temperature_sensor import get_current_temperature
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/get-temperature', methods=['GET'])
 def get_temperature():
